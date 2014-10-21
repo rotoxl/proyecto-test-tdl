ede0200326e53c519f0139a6744b2ff53b153e4f
rotoxl@gmail.com <rotoxl@gmail.com@bcefee1e-4b09-a65b-5ff8-2170d4e1db93>
2014-10-21 07:38:11 +0000

    Login with google

--- web/js/app.plugin.js
+++ web/js/app.plugin.js
@@ -1,19 +1,82 @@
-!function ($) {
+!function (jQuery) {
 
-  $(function(){
+  jQuery(function(){
 	// slim-scroll
-	$('.no-touch .slim-scroll').each(function(){
-		var $self = $(this), $data = $self.data(), $slimResize;
+	jQuery('.no-touch .slim-scroll').each(function(){
+		var $self = jQuery(this), $data = $self.data(), $slimResize;
 		$self.slimScroll($data);
-		$(window).resize(function(e) {
+		jQuery(window).resize(function(e) {
 			clearTimeout($slimResize);
 			$slimResize = setTimeout(function(){$self.slimScroll($data);}, 500);
 		});
 
-    $(document).on('updateNav', function(){
+    jQuery(document).on('updateNav', function(){
       $self.slimScroll($data);
     });
 	});
 
   });
 }(window.jQuery);
+// var options={
+// 		...
+//  	client_id: "SECRETO",
+//  	...
+// 		}
+var googleapi = {
+    authorize: function(options) {
+        var deferred = jQuery.Deferred();
+
+        //Build the OAuth consent page URL
+        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + jQuery.param({
+            client_id: options.client_id,
+            redirect_uri: options.redirect_uri,
+            response_type: 'code',
+            scope: options.scope
+        });
+
+        //Open the OAuth consent page in the InAppBrowser
+        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
+
+        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
+        //which sets the authorization code in the browser's title. However, we can't
+        //access the title of the InAppBrowser.
+        //
+        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
+        //authorization code will get set in the url. We can access the url in the
+        //loadstart and loadstop events. So if we bind the loadstart event, we can
+        //find the authorization code and close the InAppBrowser after the user
+        //has granted us access to their data.
+        jQuery(authWindow).on('loadstart', function(e) {
+            var url = e.originalEvent.url;
+            var code = /\?code=(.+)$/.exec(url);
+            var error = /\?error=(.+)$/.exec(url);
+
+            if (code || error) {
+                //Always close the browser when match is found
+                authWindow.close();
+            }
+
+            if (code) {
+                //Exchange the authorization code for an access token
+                jQuery.post('https://accounts.google.com/o/oauth2/token', {
+                    code: code[1],
+                    client_id: options.client_id,
+                    client_secret: options.client_secret,
+                    redirect_uri: options.redirect_uri,
+                    grant_type: 'authorization_code'
+                }).done(function(data) {
+                    deferred.resolve(data);
+                }).fail(function(response) {
+                    deferred.reject(response.responseJSON);
+                });
+            } else if (error) {
+                //The user denied access to the app
+                deferred.reject({
+                    error: error[1]
+                });
+            }
+        });
+
+        return deferred.promise();
+    }
+}
