79986e7bd854f311f47a60c819b3a54741ba73a8
rotoxl@gmail.com <rotoxl@gmail.com@bcefee1e-4b09-a65b-5ff8-2170d4e1db93>
2014-11-05 10:33:22 +0000

    +opciones para migrar

---
+++ trunk/scripts/convertirCaracteresExtraños.sql
@@ -0,0 +1,177 @@
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&iquest;', '¿'),
+	respuesta0=replace(respuesta0, '&iquest;', '¿'),
+	respuesta1=replace(respuesta1, '&iquest;', '¿'),
+	respuesta2=replace(respuesta2, '&iquest;', '¿'),
+	respuesta3=replace(respuesta3, '&iquest;', '¿'),
+	respuesta4=replace(respuesta4, '&iquest;', '¿'),
+	respuesta5=replace(respuesta5, '&iquest;', '¿');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&aacute;', 'á'),
+	respuesta0=replace(respuesta0, '&aacute;', 'á'),
+	respuesta1=replace(respuesta1, '&aacute;', 'á'),
+	respuesta2=replace(respuesta2, '&aacute;', 'á'),
+	respuesta3=replace(respuesta3, '&aacute;', 'á'),
+	respuesta4=replace(respuesta4, '&aacute;', 'á'),
+	respuesta5=replace(respuesta5, '&aacute;', 'á');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&Aacute;', 'Á'),
+	respuesta0=replace(respuesta0, '&Aacute;', 'Á'),
+	respuesta1=replace(respuesta1, '&Aacute;', 'Á'),
+	respuesta2=replace(respuesta2, '&Aacute;', 'Á'),
+	respuesta3=replace(respuesta3, '&Aacute;', 'Á'),
+	respuesta4=replace(respuesta4, '&Aacute;', 'Á'),
+	respuesta5=replace(respuesta5, '&Aacute;', 'Á');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&eacute;', 'é'),
+	respuesta0=replace(respuesta0, '&eacute;', 'é'),
+	respuesta1=replace(respuesta1, '&eacute;', 'é'),
+	respuesta2=replace(respuesta2, '&eacute;', 'é'),
+	respuesta3=replace(respuesta3, '&eacute;', 'é'),
+	respuesta4=replace(respuesta4, '&eacute;', 'é'),
+	respuesta5=replace(respuesta5, '&eacute;', 'é');
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&Eacute;', 'É'),
+	respuesta0=replace(respuesta0, '&Eacute;', 'É'),
+	respuesta1=replace(respuesta1, '&Eacute;', 'É'),
+	respuesta2=replace(respuesta2, '&Eacute;', 'É'),
+	respuesta3=replace(respuesta3, '&Eacute;', 'É'),
+	respuesta4=replace(respuesta4, '&Eacute;', 'É'),
+	respuesta5=replace(respuesta5, '&Eacute;', 'É');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&iacute;', 'í'),
+	respuesta0=replace(respuesta0, '&iacute;', 'í'),
+	respuesta1=replace(respuesta1, '&iacute;', 'í'),
+	respuesta2=replace(respuesta2, '&iacute;', 'í'),
+	respuesta3=replace(respuesta3, '&iacute;', 'í'),
+	respuesta4=replace(respuesta4, '&iacute;', 'í'),
+	respuesta5=replace(respuesta5, '&iacute;', 'í');
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&Iacute;', 'Í'),
+	respuesta0=replace(respuesta0, '&Iacute;', 'Í'),
+	respuesta1=replace(respuesta1, '&Iacute;', 'Í'),
+	respuesta2=replace(respuesta2, '&Iacute;', 'Í'),
+	respuesta3=replace(respuesta3, '&Iacute;', 'Í'),
+	respuesta4=replace(respuesta4, '&Iacute;', 'Í'),
+	respuesta5=replace(respuesta5, '&Iacute;', 'Í');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&oacute;', 'ó'),
+	respuesta0=replace(respuesta0, '&oacute;', 'ó'),
+	respuesta1=replace(respuesta1, '&oacute;', 'ó'),
+	respuesta2=replace(respuesta2, '&oacute;', 'ó'),
+	respuesta3=replace(respuesta3, '&oacute;', 'ó'),
+	respuesta4=replace(respuesta4, '&oacute;', 'ó'),
+	respuesta5=replace(respuesta5, '&oacute;', 'ó');
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&Oacute;', 'Ó'),
+	respuesta0=replace(respuesta0, '&Oacute;', 'Ó'),
+	respuesta1=replace(respuesta1, '&Oacute;', 'Ó'),
+	respuesta2=replace(respuesta2, '&Oacute;', 'Ó'),
+	respuesta3=replace(respuesta3, '&Oacute;', 'Ó'),
+	respuesta4=replace(respuesta4, '&Oacute;', 'Ó'),
+	respuesta5=replace(respuesta5, '&Oacute;', 'Ó');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&uacute;', 'ú'),
+	respuesta0=replace(respuesta0, '&uacute;', 'ú'),
+	respuesta1=replace(respuesta1, '&uacute;', 'ú'),
+	respuesta2=replace(respuesta2, '&uacute;', 'ú'),
+	respuesta3=replace(respuesta3, '&uacute;', 'ú'),
+	respuesta4=replace(respuesta4, '&uacute;', 'ú'),
+	respuesta5=replace(respuesta5, '&uacute;', 'ú');
+update preguntas_tests set 
+	pregunta  =replace(pregunta,   '&Uacute;', 'Ú'),
+	respuesta0=replace(respuesta0, '&Uacute;', 'Ú'),
+	respuesta1=replace(respuesta1, '&Uacute;', 'Ú'),
+	respuesta2=replace(respuesta2, '&Uacute;', 'Ú'),
+	respuesta3=replace(respuesta3, '&Uacute;', 'Ú'),
+	respuesta4=replace(respuesta4, '&Uacute;', 'Ú'),
+	respuesta5=replace(respuesta5, '&Uacute;', 'Ú');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&ntilde;', 'ñ'),
+	respuesta0=replace(respuesta0, '&ntilde;', 'ñ'),
+	respuesta1=replace(respuesta1, '&ntilde;', 'ñ'),
+	respuesta2=replace(respuesta2, '&ntilde;', 'ñ'),
+	respuesta3=replace(respuesta3, '&ntilde;', 'ñ'),
+	respuesta4=replace(respuesta4, '&ntilde;', 'ñ'),
+	respuesta5=replace(respuesta5, '&ntilde;', 'ñ');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&lsquo;', '"'),
+	respuesta0=replace(respuesta0, '&lsquo;', '"'),
+	respuesta1=replace(respuesta1, '&lsquo;', '"'),
+	respuesta2=replace(respuesta2, '&lsquo;', '"'),
+	respuesta3=replace(respuesta3, '&lsquo;', '"'),
+	respuesta4=replace(respuesta4, '&lsquo;', '"'),
+	respuesta5=replace(respuesta5, '&lsquo;', '"');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&rsquo;', '"'),
+	respuesta0=replace(respuesta0, '&rsquo;', '"'),
+	respuesta1=replace(respuesta1, '&rsquo;', '"'),
+	respuesta2=replace(respuesta2, '&rsquo;', '"'),
+	respuesta3=replace(respuesta3, '&rsquo;', '"'),
+	respuesta4=replace(respuesta4, '&rsquo;', '"'),
+	respuesta5=replace(respuesta5, '&rsquo;', '"');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&ldquo;', '"'),
+	respuesta0=replace(respuesta0, '&ldquo;', '"'),
+	respuesta1=replace(respuesta1, '&ldquo;', '"'),
+	respuesta2=replace(respuesta2, '&ldquo;', '"'),
+	respuesta3=replace(respuesta3, '&ldquo;', '"'),
+	respuesta4=replace(respuesta4, '&ldquo;', '"'),
+	respuesta5=replace(respuesta5, '&ldquo;', '"');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&rdquo;', '"'),
+	respuesta0=replace(respuesta0, '&rdquo;', '"'),
+	respuesta1=replace(respuesta1, '&rdquo;', '"'),
+	respuesta2=replace(respuesta2, '&rdquo;', '"'),
+	respuesta3=replace(respuesta3, '&rdquo;', '"'),
+	respuesta4=replace(respuesta4, '&rdquo;', '"'),
+	respuesta5=replace(respuesta5, '&rdquo;', '"');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&quot;', '"'),
+	respuesta0=replace(respuesta0, '&quot;', '"'),
+	respuesta1=replace(respuesta1, '&quot;', '"'),
+	respuesta2=replace(respuesta2, '&quot;', '"'),
+	respuesta3=replace(respuesta3, '&quot;', '"'),
+	respuesta4=replace(respuesta4, '&quot;', '"'),
+	respuesta5=replace(respuesta5, '&quot;', '"');
+
+--«
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&laquo;', '"'),
+	respuesta0=replace(respuesta0, '&laquo;', '"'),
+	respuesta1=replace(respuesta1, '&laquo;', '"'),
+	respuesta2=replace(respuesta2, '&laquo;', '"'),
+	respuesta3=replace(respuesta3, '&laquo;', '"'),
+	respuesta4=replace(respuesta4, '&laquo;', '"'),
+	respuesta5=replace(respuesta5, '&laquo;', '"');
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&raquo;', '"'),
+	respuesta0=replace(respuesta0, '&raquo;', '"'),
+	respuesta1=replace(respuesta1, '&raquo;', '"'),
+	respuesta2=replace(respuesta2, '&raquo;', '"'),
+	respuesta3=replace(respuesta3, '&raquo;', '"'),
+	respuesta4=replace(respuesta4, '&raquo;', '"'),
+	respuesta5=replace(respuesta5, '&raquo;', '"');
+--
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&gt;', '>'),
+	respuesta0=replace(respuesta0, '&gt;', '>'),
+	respuesta1=replace(respuesta1, '&gt;', '>'),
+	respuesta2=replace(respuesta2, '&gt;', '>'),
+	respuesta3=replace(respuesta3, '&gt;', '>'),
+	respuesta4=replace(respuesta4, '&gt;', '>'),
+	respuesta5=replace(respuesta5, '&gt;', '>');
+
+update preguntas_tests set 
+	pregunta  =replace(pregunta  , '&lt;', '<'),
+	respuesta0=replace(respuesta0, '&lt;', '<'),
+	respuesta1=replace(respuesta1, '&lt;', '<'),
+	respuesta2=replace(respuesta2, '&lt;', '<'),
+	respuesta3=replace(respuesta3, '&lt;', '<'),
+	respuesta4=replace(respuesta4, '&lt;', '<'),
+	respuesta5=replace(respuesta5, '&lt;', '<');
---
+++ trunk/scripts/generaScriptsMigracionDB.sh
@@ -0,0 +1,2 @@
+/Applications/MAMP/Library/bin/mysqldump -u root -p --no-data --opt tapp37 > "./EXPORT_esquemaBD.sql"
+/Applications/MAMP/Library/bin/mysqldump -u root -p --no-create-info --opt tapp37 > "./EXPORT_datosBD.sql"
