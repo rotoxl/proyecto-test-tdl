65bf6903eafc785ff1006285cb5fff78b68d34e2
rotoxl@gmail.com <rotoxl@gmail.com@bcefee1e-4b09-a65b-5ff8-2170d4e1db93>
2014-11-18 19:05:34 +0000

    Adaptación a la nueva estructura

--- trunk/recursos/createCordova.sh
+++ trunk/recursos/createCordova.sh
@@ -6,7 +6,7 @@ cd $BASE_DIR
 
 rm -rf www/*
 rsync -a -c ../lnk www
-rsync -a -c ../cordova/config.xml .
+rsync -a -c ../recursos/config.xml .
 
 cordova plugin add org.apache.cordova.inappbrowser
 cordova plugin add org.apache.cordova.contacts
@@ -17,22 +17,22 @@ cordova platform add android
 
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable
 
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-hdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-ldpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-mdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-xdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-xhdpi
+
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-hdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-ldpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-mdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-xdpi
+rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-xhdpi
+
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-hdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-hdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-ldpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-mdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-xdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-land-xhdpi
-
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-ldpi
-
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-mdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-hdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-ldpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-mdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-xdpi
-# rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-port-xhdpi
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-xhdpi
 # rm -rf /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res/drawable-xxhdpi
 
-rsync -a -c ../cordova/android/res /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res
+rsync -a -c ../recursos/android/res /Users/rotoxl/dev/proyectotest/$BASE_DIR/platforms/android/res
