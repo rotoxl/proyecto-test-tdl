a1198d05222f33e245e1d15427faa8b04b7007b7
rotoxl@gmail.com <rotoxl@gmail.com@bcefee1e-4b09-a65b-5ff8-2170d4e1db93>
2014-11-21 06:54:58 +0000

    Cuadernos MSSI terminados

--- src/migrador/migrador.js
+++ src/migrador/migrador.js
@@ -2,8 +2,17 @@ var app
 function XApp(){
 	this.txtFrom=$('#txtOrigen')
 	this.txtTo=$('#txtDestino')
-}
-XApp.prototype.cargaArchivoRemoto=function(enlace){
+	}
+XApp.prototype.init=function(){
+	// this.cargaArchivoRemoto('Cuaderno_2009_1_M_Respuestas.txt', 235, 5)
+	this.cargaArchivoRemoto('Cuaderno_2009_1_M.txt', 235, 5)
+	}
+XApp.prototype.procesa=function(data, numPregs, numResps){
+	// var ret=new RespuestasMSSI().procesa(data, numPregs, numResps)
+	var ret=new CuadernoMSSI().procesa(data, numPregs, numResps)
+	this.txtTo.val( JSON.stringify(ret) )
+	}
+XApp.prototype.cargaArchivoRemoto=function(enlace, numPregs, numResps){
 	var self=this
 
 	jQuery.ajax({
@@ -16,7 +25,7 @@ XApp.prototype.cargaArchivoRemoto=function(enlace){
 			console.info('Archivo remoto descargado')
 			
 			self.txtFrom.val(data)
-			self.procesa(data)
+			self.procesa(data, numPregs, numResps)
 			}
 		)
 	.error(
@@ -24,22 +33,16 @@ XApp.prototype.cargaArchivoRemoto=function(enlace){
 			console.error(a)
 		})
 	}
-XApp.prototype.procesa=function(data){
-	this.doProcesa(data, 110, 5)
+//////////////////
+function CuadernoMSSI(){}
+CuadernoMSSI.prototype.respuestaCorrecta=function(i){
+	var a=
+	 	[null,"2","5","2","4","4","1","4","5","1","3","1","2","4","3","1","2","3","1","2","4","2","2","5","2","4","5","","5","1","3","3","","4","5","3","4","1","3","2","1","3","5","1","4","2","3","3","3","5","3","4","5","1","5","2","4","4","4","5","2","5","3","4","2","3","2","5","2","1","4","4","3","5","3","4","1","3","1","4","3","4","3","4","5","3","5","3","2","1","5","4","3","1","2","4","3","4","2","3","3","3","5","1","3","3","5","1","3","4","2","3","4","2","4","3","4","5","1","5","5","4","3","5","","4","2","4","4","2","4","3","4","5","2","3","2","5","3","2","4","2","3","4","5","3","3","2","2","3","4","4","5","1","5","3","2","4","4","4","1","4","5","3","4","2","2","4","2","5","1","4","","4","2","3","3","3","5","3","2","2","3","3","1","2","3","3","4","5","4","4","2","1","1","5","4","3","1","4","5","3","2","4","5","3","2","4","5","4","2","1","5","3","1","3","4","3","3","3","4","3","4","","4","3","3","2","2","1","4","1","2","4","4","2"]
+
+	return Number(a[i+1])-1
 	}
-XApp.prototype.faltaIDPregunta=function(i, p){
-	this.lRevisar.push ('>>'+i+'(es preg'+p+')' )
-	}
-XApp.prototype.revisar=function(i, m){
-	this.lRevisar.push(i)
-	console.warn('>> revisar Linea '+i+': '+m)
-	}
-XApp.prototype.empiezaPor12345=function(s){
-	var l=['1', '2', '3', '4', '5', ]
-	 return l.indexOf(s)>-1
-}
-XApp.prototype.doProcesa=function(data, numPregs, numRespsPorPreg){
-	var longMinimaResp=6, desfase=0
+CuadernoMSSI.prototype.procesa=function(data, numPregs, numRespsPorPreg){
+	var longMinimaResp=5, desfase=0
 	var preguntasLlevanNumero=true, respsLlevanNumero=true
 	var sepResps='. '
 
@@ -68,7 +71,9 @@ XApp.prototype.doProcesa=function(data, numPregs, numRespsPorPreg){
 		var p={
 			filapreg:filapreg,
 			id:i+1,
-			q:tpreg,
+
+			cd_pregunta:i,
+			pregunta:tpreg,
 			}
 
 		if ( tpreg.slice(-1)=='-' ){
@@ -121,11 +126,11 @@ XApp.prototype.doProcesa=function(data, numPregs, numRespsPorPreg){
 			if (trozos[0]!=(j+1) )
 				throw 'No se puede continuar sin corregir error en respuesta '+(i+1)+':'+j+', línea '+fnFilaResp(i,j)
 
-			p['a'+j]=tresp
+			p['respuesta'+j]=tresp
 			}
 
-		
-		ret.push(p)
+		p.cd_respuestacorrecta=this.respuestaCorrecta(i)
+		ret.push( this.convierteCarRaros(p) )
 		
 		console.log(p)
 		console.log('-------------------------')	
@@ -135,21 +140,73 @@ XApp.prototype.doProcesa=function(data, numPregs, numRespsPorPreg){
 	if (ultimaLineaProc==t.length)
 		console.info('El num de lineas parece estar bien')
 	else
-		console.error('El num de lineas parece estar mal')
+		console.error('El num de lineas parece estar mal, elimina las línas vacías al final del doc')
 	console.log('-------------------------')
 
-	// console.log('Lineas <a revisar:')
-	// console.log(this.lRevisar)
+	return ret
 	}
-XApp.prototype.convierteCarRaros=function(p){
-	p.q=this.__convierteCarRaros(p.q)
-	p.a0=this.__convierteCarRaros(p.a0)
-	p.a1=this.__convierteCarRaros(p.a1)
-	p.a2=this.__convierteCarRaros(p.a2)
-	p.a3=this.__convierteCarRaros(p.a3)
-	p.a4=this.__convierteCarRaros(p.a4)
+CuadernoMSSI.prototype.faltaIDPregunta=function(i, p){
+	this.lRevisar.push ('>>'+i+'(es preg'+p+')' )
+	}
+CuadernoMSSI.prototype.revisar=function(i, m){
+	this.lRevisar.push(i)
+	console.warn('>> revisar Linea '+i+': '+m)
+	}
+CuadernoMSSI.prototype.empiezaPor12345=function(s){
+	var l=['1', '2', '3', '4', '5', ]
+	return l.indexOf(s)>-1
+	}
+CuadernoMSSI.prototype.convierteCarRaros=function(p){
+	p.pregunta 	=this.__convierteCarRaros(p.pregunta,   p.id)
+	p.respuesta0=this.__convierteCarRaros(p.respuesta0, 1)
+	p.respuesta1=this.__convierteCarRaros(p.respuesta1, 2)
+	p.respuesta2=this.__convierteCarRaros(p.respuesta2, 3)
+	p.respuesta3=this.__convierteCarRaros(p.respuesta3, 4)
+	p.respuesta4=this.__convierteCarRaros(p.respuesta4, 5)
+	return p
+	}
+CuadernoMSSI.prototype.__convierteCarRaros=function(s, id){
+	// ñ, á, é, í, ó, ú, - 
+	
+	//quitamos el num de delante, si lo tiene
+	var pref=id+'. '
+	if (s.indexOf(pref)==0)
+		s=s.substring(pref.length)
 	return s
-}
-XApp.prototype.__convierteCarRaros=function(s){
+	}
+//////////////////
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        	}
+        throw message; // Fallback
+    	}
+	}
+//////////////////
 
+function RespuestasMSSI(){}
+RespuestasMSSI.prototype.procesa=function(data, numPregs, numRespsPorPreg){
+	var t=data.split('\r')
+	if (t.length<3)
+		t=data.split('\n')
+
+	var ret=[]
+	for (var i=0; i<t.length; i++){
+		var l=t[i]
+
+		var trozos=l.split('\t')
+		for (var j=0; j<trozos.length; j=j+2){
+			var p=trozos[j]
+			var r=trozos[j+1]
+
+			assert(p>0 && p<numPregs+1, '¿numPreguntas=='+numPregs+'?')
+			assert(r=='' || (r>0 && r<(numRespsPorPreg+1)), '¿Respuestas=='+numRespsPorPreg+'?')
+
+			ret[p]=r
+			}
+		}
+	return ret
 }
+
